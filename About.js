7448b6162d3df6c6a00d43791c01f77933948af7
ghazal-aroosa <65502534+ghazal-aroosa@users.noreply.github.com>
2020-08-17 09:02:33 +0500

    makes changes .travis.yml and make file About.js and About.test.js

---
+++ About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+               hello About
+               <input type="text"/>
+        </div>
+    )
+}
+export default About
