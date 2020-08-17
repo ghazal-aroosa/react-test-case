0fc5a4b0650e2f0d2ae812172ec29b969b7caa81
ghazal-aroosa <65502534+ghazal-aroosa@users.noreply.github.com>
2020-08-17 07:21:54 +0500

    add on master branch

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/hello world/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('text case', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/hello piaic/i);
+  expect(linkElement).toBeInTheDocument();
+});
